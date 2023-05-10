b31c4e83f95efaccf706c0835e9b9d1e8368c546
Ahmed Mohamed Lotfi <103373734+ahmed-lotfi@users.noreply.github.com>
2023-05-10 18:12:17 +0300

    Create Task4.cpp

---
+++ Task4.cpp
@@ -0,0 +1,45 @@
+/*
+       # |##
+      ###|##
+     ####|##
+    #####|##
+   ##  ##|##
+  ##---##|##
+ ##    ##|#######
+##     ##|#######
+@Ahmed-Lotfi
+-Every day can be a new beginning:)
+*/
+#include <iostream>
+using namespace std;
+/*OOP*/
+/*
+Design Review
+struct BankCustomer {
+string name;
+string address;
+string mobile;
+string birth_of_date;
+int rectangle width;
+string favorite_movie;
+string favorite_color;
+string favorite_actor;
+string car_model;
+string favorite_food;
+};
+*/
+// I see that in this code there's many things aren't important
+struct BankCustomer {
+    string name;
+    string address;
+    string mobile;
+    string birth_of_date;
+    string national_id;
+    string email;
+    int account_balance;
+};
+/*****/
+int main()
+{
+  
+}
